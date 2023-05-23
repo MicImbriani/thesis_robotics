ee1310c9119a3f9549c32cc563dc3f231e2bf95c
MicImbriani <imbr.mic@gmail.com>
2023-05-23 10:01:22 +0200

    Added v3 backup

---
+++ v3_backup/learn_robot.py
@@ -0,0 +1,209 @@
+from random import choice
+from numpy import array, add
+from math import cos, sin, pi
+
+from utils.constants import *
+from utils.counter import Counter
+from utils.utils import get_direction_from_dir, get_direction_from_heading, get_relative_directions, get_heading_from_direction
+from utils.qlearn_utils import get_spacing_from_dist
+
+from state import State
+from .robot import Robot
+
+class LearnRobot(Robot):
+    def __init__(self, game_map, startpos=None):
+        super().__init__(game_map, startpos)
+        self.name = "LearnRobot"
+        self.history = [self.position]
+        _, self.collisions = self._sense_obstacles()
+        # MOVEMENT SPEEDS
+        self.max_speed = 50
+        self.min_speed = -10
+        self.acc_increment = 10
+        self.dec_increment = 10
+
+        # Q-LEARN
+        self.q_table = Counter()
+        self.last_state = []
+        self.last_action = []
+        self.current_dists = {}
+        self.discount_factor = 0.8
+        self.discounted_total_rewards = 0
+
+    def init_state(self):
+        self.state = State(self, self.other_robots)
+    
+
+    ################ UPDATE ################
+    def update(self, dt):
+        """ TODO: update is called from the Swarm object.
+        Need to make it so that the update method executes the whole
+        learning for the robot.
+        The current hierarchy is:
+        Main: manages the iteration of the learning 
+        > Q-Learn(Simulation): manages each tick in a simulation;
+            at each tick, the Swarm is used
+        >> Swarm: execs the training step of all the robots
+        >> > LearnRobot: Executes the individual Q-learn
+        >>>>> State: gets updated to give information about the current state
+        """ 
+        _, self.collisions = self._sense_obstacles()
+        self.kinematics(dt)
+        self.store_distances()
+        # Add current position to the history of robot's positions
+        self.history.append(self.position)
+
+
+    def kinematics(self, dt):
+        v = (self.speedL + self.speedR) / 2
+        # equation space-time-velocity
+        # S(t) = S(t-1) + k*v(t)
+        self.x += v * cos(self.heading) * dt
+        self.y -= v * sin(self.heading) * dt
+
+
+
+    ################ STATE ################
+
+    def get_new_state(self, current_distances):
+        print(self.heading)
+        return self.state.get_current_state(current_distances)
+
+
+    ################   ACTIONS   ################
+
+    def accelerate(self):
+        # RIGHT
+        if self.speedL + self.acc_increment <= self.max_speed:
+            self.speedL += self.acc_increment
+        else: self.speedL = self.max_speed
+        # LEFT
+        if self.speedR + self.acc_increment <= self.max_speed: 
+            self.speedR += self.acc_increment
+        else: self.speedR = self.max_speed
+
+
+    def decelerate(self):
+        # RIGHT
+        if self.speedL - self.dec_increment >= self.min_speed:
+            self.speedR -= self.dec_increment
+        else: self.speedR = self.min_speed
+        # LEFT
+        if self.speedL - self.dec_increment >= self.min_speed:
+            self.speedL -= self.dec_increment
+        else: self.speedL = self.min_speed
+
+
+    def get_legal_actions(self):
+        """GET LEGAL GLOBAL ACTIONS"""
+        new_collisions = self.collisions[4:] + self.collisions[:4]
+        # LOCAL_DIRECTIONS = [STRAIGHT, STRAIGHT_LEFT, LEFT, BEHIND_LEFT, BEHIND, BEHIND_RIGHT, RIGHT, STRAIGHT_RIGHT]
+        my_dir = get_direction_from_heading(self.heading)
+        dir_id = GLOBAL_DIRECTIONS.index(my_dir)
+        legal_actions = [STRAIGHT]
+        for idx, action in enumerate(GLOBAL_DIRECTIONS):
+            if not new_collisions[idx - dir_id]:
+                legal_actions.append(action)
+        # Accelerate and decellerate are always legal
+        return legal_actions
+
+
+    # Given list of possible actions and the current state, returns the best action
+    def get_action(self, state, possible_actions):
+        state = state if state is not None else self.state.current_state
+        action = self.get_best_action(state, possible_actions)
+        return action
+
+
+    # Given a state and possible actions, returns the action of the highest Q value
+    def get_best_action(self, state, possible_actions):
+        tmp = Counter()
+        for action in possible_actions:
+            if self.id == 1:
+                print(f"ACTION: {action},    VALUE: {self.get_Q_value(state, action)}")
+            tmp[action] = self.get_Q_value(state, action)
+        print(f"ARGMAX {tmp.argMax()}")
+        return tmp.argMax()
+
+
+    def take_next_action(self, next_action):
+        if next_action == ACCELERATE:
+            self.accelerate()
+        elif next_action == DECELERATE:
+            self.decelerate()
+        elif next_action == STRAIGHT:
+            return # heading stays the same
+        else:
+            # HARDCODED HEADING CHANGE
+            self.heading = get_heading_from_direction(next_action)
+
+
+    def _get_new_direction(self, next_action):
+        relative_direction = get_relative_directions(
+            self.current_direction, next_action)
+        new_direction = add(array(self.current_direction),
+                            array(relative_direction))
+
+        def treshold(x):
+            sign = 1 if x >= 0 else -1
+            return 1*sign if x > 1 or x < -1 else x
+        new_direction = list(map(treshold, new_direction))
+        return get_direction_from_dir((new_direction[0], new_direction[1]))
+
+
+    @property
+    def current_direction(self):
+        return get_direction_from_heading(self.heading)
+
+
+
+
+    ################ Q-LEARN ################
+    
+    # Update the Q value of a given state-action entry
+    def update_Q(self, state, action, new_Q):
+        self.q_table[str([state, action])] = new_Q
+        # MAYBE REWRITE QLEARN EQUATION
+
+
+    # Given a state and possible actions, returns the highest Q value
+    def get_max_Q(self, state, possible_actions):
+        q_list = []
+        for action in possible_actions:
+            if action == 1 or action == -1:
+                continue
+            q_list.append(self.get_Q_value(state, action))
+            # if self.id == 1:
+            #     print(f"ACTION: {action},    VALUE: {self.get_Q_value(state, action)}")
+        return 0 if not q_list else max(q_list)
+
+
+    # Return Q-value of a given state-action pair
+    def get_Q_value(self, state, action):
+        if state == []:
+            return 0
+        return self.q_table[str([state, action])]
+    
+    def compute_reward(self, current_distances, dist_to_endpoint):
+        # Distance to end goal. Closer -> more points
+        reward = 1/ dist_to_endpoint * 30000
+        # Reward if robots stay in range of each other,
+        # penalize if too far or too close
+        spacings = list(get_spacing_from_dist(current_distances).values())
+        for robot_spacing in spacings:
+            if robot_spacing == TOO_FAR:
+                reward -= 10
+            elif robot_spacing == TOO_CLOSE:
+                reward -= 10
+            elif robot_spacing == IN_RANGE:
+                reward += 100
+            else:
+                raise Exception("Error when converting distances to spacing. Returned value is not TOO_FAR, TOO_CLOSE or IN_RANGE")
+        # Penalize every second to encourage faster solutions
+        reward -= 1
+        self._update_discounted_total_rewards(reward)
+        return reward
+
+
+    def _update_discounted_total_rewards(self, reward):
+        self.discounted_total_rewards += self.discount_factor * reward
---
+++ v3_backup/main.py
@@ -0,0 +1,295 @@
+import os
+import pickle
+import argparse
+from random import choice
+from copy import deepcopy
+from statistics import mean
+from numpy import array,argsort
+from multiprocessing import Pool
+
+from utils.qlearn_utils import save_element, load_policy, make_plots, \
+    store_distances_logs, sim_duration, load_element
+
+from q_learn import QLearn
+from simulation import Simulation
+import time
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--version", help="Version of the simulation", type=int)
+parser.add_argument("-d", "--demo", help="Demo", action='store_true', default=False)
+parser.add_argument("--headless", help="Whether to create and show display", action='store_true', default=False)
+parser.add_argument("--resume", help="Resume training", action='store_true', default=False)
+
+args = parser.parse_args()
+
+if args.headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+
+# Q-LEARNING 
+Q_LEARN = True
+training_speed = 10
+sim_duration = sim_duration
+
+# Parameters
+exploration_rho = 0.3
+lr_alpha = 0.7
+discount_rate_gamma = 0.9
+train_iterations = 1000
+RANDOM_START = False
+STRAIGHT_START = True
+
+# MULTI-VERSE
+learning_worlds = 100
+formation_discount = 0.9
+trajectory_discount = 0.7
+
+
+"""
+PROGRESS
+-1: Demo
+0: Simple running of the sim
+1: Simple q-learning
+2: Q-learn with world info exchange
+"""
+PROGRESS = 2 if args.version is None else args.version
+PROGRESS = -1 if args.demo else PROGRESS
+RESUME = True if args.resume else False
+
+
+if __name__ == "__main__" and PROGRESS == 0:
+    if not Q_LEARN:
+        my_sim = Simulation()
+        my_sim.run()
+    else:
+        def train(n):
+            print("Learning World", n)
+            for i in range(train_iterations):
+                print(f"Iterations {i}")
+                # Setup
+                my_sim = QLearn(lr_alpha, discount_rate_gamma,
+                                exploration_rho, training_speed)
+                if i != 0:
+                    my_sim.set_Q_tables(last_q_tables)
+
+                # Start
+                my_sim.run()
+
+                # Save results
+                last_q_tables = my_sim.get_Q_tables()
+                
+
+                # Reset
+                del my_sim
+
+        with Pool(learning_worlds) as p:
+            p.map(train, list(range(learning_worlds)))
+
+
+if __name__ == "__main__" and PROGRESS == 0:
+    if not Q_LEARN:
+        my_sim = Simulation()
+        my_sim.run()
+    else:
+        for i in range(train_iterations):
+            print(f"Iterations {i}")
+            # Setup
+            my_sim = QLearn(lr_alpha, discount_rate_gamma,
+                            exploration_rho, training_speed)
+            if i != 0:
+                my_sim.set_Q_tables(last_q_tables)
+
+            # Start
+            my_sim.run()
+
+            # Save results
+            last_q_tables = my_sim.get_Q_tables()
+            
+
+            
+
+            # Reset
+            del my_sim
+
+
+
+if __name__ == "__main__" and PROGRESS==1:
+    if not Q_LEARN:
+        my_sim = Simulation()
+        my_sim.run()
+    else:
+        def one_iter_funct(previous_Q_table):
+            global i
+            # Setup
+            my_sim = QLearn(lr_alpha, discount_rate_gamma,
+                            exploration_rho, training_speed)
+            if i != 0: my_sim.set_Q_tables(previous_Q_table)
+            # Start
+            my_sim.run()
+            # Store
+            tables = my_sim.get_Q_tables()
+            # Reset
+            del my_sim
+            return tables
+
+        previous_Q_tables = [[] for _ in range(learning_worlds)]
+        for i in range(train_iterations):
+            print(f"Iterations {i}")
+
+            # Stup and run all learning worlds in parallel
+            with Pool(learning_worlds) as p:
+                previous_Q_tables = p.map(one_iter_funct, previous_Q_tables)
+
+            # Perform exchange of info across worlds
+
+            # Save results
+            
+
+
+
+
+if __name__ == "__main__" and PROGRESS == 2:
+    if not Q_LEARN:
+        my_sim = Simulation()
+        my_sim.run()
+    else:
+        def one_iter_funct(previous_Q_table):
+            global i, last_iter, local_sum_rewards, RANDOM_START, STRAIGHT_START
+            # Setup
+            my_sim = QLearn(lr_alpha, discount_rate_gamma,
+                            exploration_rho, training_speed)
+            if i != 0:
+                my_sim.set_Q_tables(previous_Q_table)
+            # Start
+            random_start = True if i <= train_iterations/100 and RANDOM_START else False
+            my_sim.run(random_start, STRAIGHT_START)
+            # Store
+            formation_disr, traj_disr = my_sim.compute_world_score(
+                                                        formation_discount,
+                                                        trajectory_discount)
+            tables = my_sim.get_Q_tables()
+            # Rewards
+            rewards = my_sim.total_rewards
+            # Distances
+            dists_avgs = store_distances_logs(my_sim.formation.dists,
+                                         my_sim.swarm.robots)
+
+            # Reset
+            del my_sim
+            return formation_disr, traj_disr, tables, rewards, dists_avgs
+
+        if RESUME:
+            # Variables needed for storing the information
+            tot_avg_rewards = load_element('./TRAINED_FILES/tot_avg_rewards')
+            tot_min_rewards = load_element('./TRAINED_FILES/tot_min_rewards')
+            tot_max_rewards = load_element('./TRAINED_FILES/tot_max_rewards')
+            all_dists_logs = load_element('./TRAINED_FILES/all_dists_logs')
+            # Variables needed for executing the various learning worlds
+            info_exch = load_element('./TRAINED_FILES/info_exch')
+            info_exch_counter = load_element('./TRAINED_FILES/info_exch_counter')
+            previous_Q_tables = load_element("./TRAINED_FILES/trained_controller'")
+            iter_counter = load_element("./TRAINED_FILES/iter_counter'")
+        else:
+            # Variables needed for storing the information
+            tot_avg_rewards = []
+            tot_min_rewards = []
+            tot_max_rewards = []
+            all_dists_logs = {(0,1):[], (0,2):[], (1,2):[]}
+            # Variables needed for executing the various learning worlds
+            info_exch = []
+            previous_Q_tables = [[] for _ in range(learning_worlds)]
+            info_exch_counter = 0
+            iter_counter = 0
+        last_iter = False
+
+        # ----------- TRAINING ITERATIONS -----------
+        for i in range(train_iterations):
+            print()
+            print(f"Iterations {i}")
+            if i == train_iterations - 1:
+                last_iter = True
+            iter_counter += 1
+
+            # Reset local sum of rewards
+            local_sum_rewards = 0
+
+            # Stup and run all learning worlds in parallel
+            with Pool(learning_worlds) as p:
+                world_scores = p.map(one_iter_funct, previous_Q_tables)
+
+            # Store info of rewards to global counter
+            rewards_list = [world[-2] for world in world_scores]
+            tot_avg_rewards.append(mean(rewards_list))
+            tot_min_rewards.append(min(rewards_list))
+            tot_max_rewards.append(max(rewards_list))
+
+            # Store info of distances log
+            dists_list = [world[-1] for world in world_scores][0] \
+                if learning_worlds == 1 else [world[-1] for world in world_scores]
+            
+            if learning_worlds == 1:
+                [all_dists_logs[pair].append(pair_dist) for pair, pair_dist in zip(all_dists_logs, dists_list)]
+            else: 
+                [all_dists_logs[pair].append(pair_dist[0]) for pair, pair_dist in zip(all_dists_logs, dists_list)]
+
+            # Compute best performing world
+            all_world_scores = []
+            world_results_dict = {idx: world[2] for idx, world in enumerate(world_scores)}
+            for idx, world in enumerate(world_scores):
+                world_list = list(world)
+                world_list.insert(0, idx)
+                all_world_scores.append(world_list)
+
+            all_world_scores_copy = deepcopy([x[:3] for x in all_world_scores])
+            array_all_world_scores = array(all_world_scores_copy)
+            sort_by_form = array_all_world_scores[array_all_world_scores[:, 1].argsort()].tolist()
+            sort_by_traj = array_all_world_scores[array_all_world_scores[:, 2].argsort()].tolist()
+            
+            # Check if the BEST FORMation world is also the BEST VARIANCE world
+            if int(sort_by_form[0][0]) == int(sort_by_traj[0][0]):
+                # If so, do the exchange...
+                info_exch.append(i)
+                info_exch_counter += 1
+                print(f"EXCHANGE {info_exch_counter}")
+                previous_Q_tables = [deepcopy(world_results_dict[sort_by_form[0][0]]) for _ in range(learning_worlds)]
+            # otherwise, just use previous iteration's q-tables.
+            else:
+                previous_Q_tables = [world[2] for world in world_scores]
+            print(f"Q-TABLE {len(previous_Q_tables[0][0])}")
+
+            # Save results
+            if i % 10 == 0:
+                save_element(previous_Q_tables, './TRAINED_FILES/trained_controller')
+                save_element(tot_avg_rewards, './TRAINED_FILES/tot_avg_rewards')
+                save_element(tot_min_rewards, './TRAINED_FILES/tot_min_rewards')
+                save_element(tot_max_rewards, './TRAINED_FILES/tot_max_rewards')
+                save_element(all_dists_logs, './TRAINED_FILES/all_dists_logs')
+                save_element(info_exch, './TRAINED_FILES/info_exch')
+                save_element(info_exch_counter, './TRAINED_FILES/info_exch_counter')
+                save_element(iter_counter, './TRAINED_FILES/iter_counter')
+
+            # ON END
+            filemode = "w" if i == 0 else "a"
+            with open("test_output.txt", filemode) as myfile:
+                myfile.write(f"{iter_counter} total iterations,"
+                            f"INFO EXCHANGED {info_exch_counter} TIMES.")
+                myfile.write("\n")
+
+        print("LOGS", all_dists_logs)
+        print("REWARDS", tot_avg_rewards)
+        print("INFO EXCHANGED: ", info_exch_counter)
+
+
+if __name__ == "__main__" and PROGRESS == -1:
+    q_tables = load_element("./TRAINED_FILES/trained_controller'")
+    ALL = False
+    if not ALL:
+        q_tables = [choice(q_tables)]
+        print(len(q_tables))
+    for world_q_tables in q_tables:
+        # Setup
+        my_sim = QLearn(alpha=0, gamma=discount_rate_gamma,
+                        rho=0, training_speed=1)
+        # Load Q-tables
+        my_sim.set_Q_tables(world_q_tables)
+        # Start
+        my_sim.run(False, False)
