6d95e779576df6024b4f28bfce49fc3274a9f73b
MicImbriani <imbr.mic@gmail.com>
2023-05-27 13:42:08 +0200

    added stuff to print results in model-based sim

--- base/simulation.py
+++ base/simulation.py
@@ -1,8 +1,10 @@
 import pygame
+from math import sqrt
 from time import sleep
 import matplotlib.pyplot as plt
 
 from utils.dimensions import *
+from utils.utils import distance
 from utils.simulation_utils import check_stop_game, make_plots
 
 from robots.robot import Robot
@@ -19,6 +21,9 @@ class Simulation:
     def __init__(self):
         self.setup_swarm()
         self.setup_simulation()
+        r1, r2, r3 = [robot.position for robot in self.swarm]
+        self.base_formation_area = self.compute_triangle_area(r1, r2, r3)
+        self.all_form_areas = []
 
 
     def setup_swarm(self):
@@ -61,6 +66,7 @@ class Simulation:
             self.gfx.update()
             pygame.display.update()
             self.formation.get_distances()
+            self.all_form_areas.append(self.formation_disruption())
 
     def run(self):
         tick = pygame.time.get_ticks()
@@ -70,3 +76,14 @@ class Simulation:
                 robot.init_state()
 
         self.update(tick)
+
+    def compute_triangle_area(self, p1, p2, p3):
+        l1 = distance(p1, p2)
+        l2 = distance(p2, p3)
+        l3 = distance(p1, p3)
+        s = (l1 + l2 + l3) / 2
+        return sqrt(abs(s * (s-l1)*(s-l2)*(s-l3)))
+    
+    def formation_disruption(self):
+        r1, r2, r3 = [robot.position for robot in self.swarm]
+        return abs(self.compute_triangle_area(r1, r2, r3) - self.base_formation_area)
